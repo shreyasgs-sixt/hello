be671d4d28b8040049f0835e183b370c56e7a2f5
shreyasgs-sixt <shreyas.gs@sixt.com>
2023-01-23 07:57:48 +0530

    add function

--- src/main/java/com/demo/hello/HelloController.java
+++ src/main/java/com/demo/hello/HelloController.java
@@ -1,6 +1,9 @@
 package com.demo.hello;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String welcome(){
         return "Hello Shreyas GS\nWelcome to SIXT";
+    }
+
+    @GetMapping("/square")
+    public ResponseEntity<Integer> getSquare(@RequestParam int n) {
+        return ResponseEntity.ok(n*n);
     }
 }
